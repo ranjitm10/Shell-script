73cc8d6ea5644988c2ea1a4c1d5cac91fb7e3692
Ranjit Mohite <ranjitmohite00@gmail.com>
2023-03-30 11:34:54 +0530

    first commit

---
+++ helloworld.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Hello World"
+echo "this is our first shellscript."
+pwd
---
+++ printdc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# shebang
+echo this is Ranjit Mohite
+echo 'this is our first shellscript'
+echo -e "\033[0;31m fail message"
+echo -e "\033[0;32m success message"
+echo -e "\033[0;33m warning message here"
